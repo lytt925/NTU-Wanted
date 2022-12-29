c7649c66889c5fe3ccdbab37cd76d737f534b931
Yen-Ting Li <vincent20000925@gmail.com>
2022-12-29 20:52:24 +0800

    fix models typo

--- backend/src/models/comment.js
+++ backend/src/models/comment.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
     name: { type: String, required: [true, 'User Name field is required.'] },
     content: { type: String, required: [true, 'content field is required.'] },
-    reply: [{ name: { type: mongoose.Types.ObjectId, ref: 'User' }, commet: { type: String } }]
+    reply: [{ name: { type: mongoose.Types.ObjectId, ref: 'User' }, comment: { type: String } }]
 }, {
     collection: 'Comment',
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
