9ac417881d4da7349178df68a0676cb1b70cdea0
Yen-Ting Li <vincent20000925@gmail.com>
2022-12-29 16:51:53 +0800

    mongoose model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -5,8 +5,11 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     name: { type: String, required: [true, 'User Name field is required.'] },
     email: { type: String, required: [true, 'email field is required.'] }
+}, {
+    collection: 'User',
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 })
 
 const UserModel = mongoose.model('User', UserSchema)
 
-export default Experiment;
+export default UserModel;
