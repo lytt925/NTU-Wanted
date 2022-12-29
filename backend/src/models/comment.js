b99a15631fb5108069a936015d4e37ed5e02dfd9
Alan Hsiao <alanhsiao@AlandeMacBook-Pro.local>
2022-12-29 18:31:33 +0800

    CommentModel_12291831

---
+++ backend/src/models/comment.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    name: { type: String, required: [true, 'User Name field is required.'] },
+    content: { type: String, required: [true, 'content field is required.'] }
+}, {
+    collection: 'Comment',
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+})
+
+const CommentModel = mongoose.model('Comment', CommentSchema)
+
+export default CommentModel;
