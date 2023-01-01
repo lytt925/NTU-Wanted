dfc947ba63a5f39b2e2a2c6f5ab087916b29a4e7
Chia-Chi-Shen <musicsusanshen@gmail.com>
2023-01-01 13:42:43 +0800

    postForm

--- backend/src/models/experiment.js
+++ backend/src/models/experiment.js
@@ -2,24 +2,33 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 const ExpSchema = new Schema({
+
+    //基本資料
     creator: { type: mongoose.Types.ObjectId, ref: 'User' },
     id: { type: String, required: [true, 'id field is required.'] },
     title: { type: String, required: [true, 'Title field is required.'] },
     location: { type: String, required: [true, 'Location field is required.'] },
     experimenter: { type: String, required: [true, 'experimenter field is required.'] },
-    length: { type: String, required: true },
-    phone: { type: String, required: true },
+    phone: { type: String },
     email: { type: String, required: true },
+
+    //受試者條件
+    age: { upper: { type:Number }, lower: { type:Number } },
+    requirements: { type: String },
+
+    //研究內容
+    length: { type: String, required: true },
     time: { type: String, required: true },
     timeRange: { from: { type: String, required: true }, to: { type: String, required: true } },
     introduction: { type: String, required: [true, 'Introduction field is required.'] },
-    memo: { type: String, required: true },
-    reward: { type: String, required: true },
+    memo: { type: String },
+    reward: { type: String },
     link: { type: String },
     typeTags: [{ type: String, required: true },],
     rewardTags: [{ type: String, required: true },],
     locationTags: [{ type: String, required: true },],
     otherTags: [{ type: String }],
+
     // comments: [{ type: mongoose.Types.ObjectId, ref: 'Comment' }],
     comments: [{
         user: { type: mongoose.Types.ObjectId, ref: 'User' },
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import ExpRouter from './expList'
 import InfoRouter from './expInfo'
 import commentRoute from './comment'
+import postFormRouter from './postForm'
 
 const router = Router();
 router.get('/api/getExpList', ExpRouter.getExpList);
@@ -9,5 +10,6 @@ router.use('/api/getInfo', InfoRouter.getInfo);
 router.use('/api/getCommentsByExpId', commentRoute.GetCommentsByExpId);
 router.post('/api/createComment', commentRoute.CreateComment);
 router.post('/api/createReply', commentRoute.CreateReply);
+router.post('/api/postForm', postFormRouter.createInfo);
 
 export default router;
