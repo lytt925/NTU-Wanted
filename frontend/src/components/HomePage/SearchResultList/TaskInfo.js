231fa20133672e5fecbb5be9657040d745df0256
Yen-Ting Li <vincent20000925@gmail.com>
2023-01-04 00:09:18 +0800

    restore like

--- frontend/src/components/HomePage/SearchResultList/TaskInfo.js
+++ frontend/src/components/HomePage/SearchResultList/TaskInfo.js
@@ -26,21 +26,17 @@ const List = styled('ul')({
 const TaskInfo = ({ task, liked }) => {
 
     const { email, setLikedList } = useUser()
-    const [likeThis, setLikethis] = useState(liked)
 
     const handleLike = async (e, expId, action) => {
         e.stopPropagation()
-        if (action === 'unlike') setLikethis(false)
-        else setLikethis(true)
         const { data: { message, likedList: newLikedList } } =
             await axios.post("/updateLikeList", { email, expId, action })
         if (message === 'success') {
-            setLikedList(newLikedList)
         }
         else {
-            setLikethis(!likeThis)
             console.log(action, "failed ")
         }
+        setLikedList(newLikedList)
     }
 
     const navigate = useNavigate();
@@ -57,7 +53,7 @@ const TaskInfo = ({ task, liked }) => {
                 <Typography className="Title" variant="h6" sx={{ "&:hover": { color: '#4267B2' } }} onClick={() => ToExp(task.id)}>
                     {task.title}
                 </Typography>
-                {likeThis ? <Favorite onClick={(e) => handleLike(e, task._id, 'unlike')} sx={{ color: 'red' }} />
+                {liked ? <Favorite onClick={(e) => handleLike(e, task._id, 'unlike')} sx={{ color: 'red' }} />
                     : <FavoriteBorder onClick={(e) => handleLike(e, task._id, 'like')} />}
             </Box>
             <List>
