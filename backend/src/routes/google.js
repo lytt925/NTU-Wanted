b2ad810bfa906c1c1f7c0c5bbf748d104bdeed44
Alan Hsiao <alanhsiao@AlandeMacBook-Pro.local>
2023-01-03 22:17:14 +0800

    google_key

--- backend/src/routes/google.js
+++ backend/src/routes/google.js
@@ -2,6 +2,6 @@ import dotenv from 'dotenv-defaults'
 dotenv.config();
 
 exports.getKey = async (req, res) => {
-    console.log('key', process.env.GOOGLE_KEY)
+    // console.log('key', process.env.GOOGLE_KEY)
     res.status(200).send(process.env.GOOGLE_KEY);
 }
