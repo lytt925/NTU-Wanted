b62595707563b5c877dbb85dbb8645b6017b12d3
Yen-Ting Li <vincent20000925@gmail.com>
2023-01-04 21:01:40 +0800

    databackup.json

--- backend/src/routes/expList.js
+++ backend/src/routes/expList.js
@@ -1,6 +1,7 @@
 import ExperimentModel from '../models/experiment'
 import dotenv from 'dotenv-defaults'
 dotenv.config();
+import * as fs from 'fs';
 
 const isOverLap = (rangeStart, rangeEnd, thisStart, thisEnd) => {
     let startDay = new Date(rangeStart).getTime();
@@ -46,6 +47,11 @@ exports.getExpList = async (req, res) => {
         typeTagsSelected,
     } = req.query
     const data = await searchExp(searchTitle, locationTagsSelected, timeRange, rewardTagsSelected, typeTagsSelected,)
+    fs.writeFile("data.json", JSON.stringify(data), function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
     if (data) {
         res.status(200).send({ message: 'success', contents: data, });
     } else {
